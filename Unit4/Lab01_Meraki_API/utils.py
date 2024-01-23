75539403e8b780635916e1a7a7ce9e6b0a98cadb
Jackal <ccxxll23@163.com>
2024-01-23 14:58:11 +0800

    updated

--- Unit4/Lab01_Meraki_API/utils.py
+++ Unit4/Lab01_Meraki_API/utils.py
@@ -1,10 +1,9 @@
-import os
 from sqlalchemy import create_engine
 
-mysql_user = os.environ.get('MYSQLUSER')
-mysql_pass = os.environ.get('MYSQLPASS')
-mysql_host = "192.168.1.50"
-mysql_db = "Meraki"
+mysql_user = "你的MySQL用户名"
+mysql_pass = "你的MySQL密码"
+mysql_host = "你的MySQL数据库IP地址"
+mysql_db = "你的MySQL数据库名称"
 
 def sql_writer(pd_data, tb_name, exists='replace'):
     sql_con = create_engine(f"mysql+pymysql://{mysql_user}:{mysql_pass}@{mysql_host}:3306/{mysql_db}")
