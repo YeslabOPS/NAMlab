1db3bf0b8c7a95f22cd25660816e84a59bae068f
Jackal <ccxxll23@163.com>
2024-07-30 15:32:30 +0800

    updated U$

---
+++ U04/GRPC/grpc_client.py
@@ -0,0 +1,15 @@
+import grpc
+import arphello_pb2
+import arphello_pb2_grpc
+
+def run():
+    #客户端实例化stub
+    connect = grpc.insecure_channel('localhost:10050')
+    stub = arphello_pb2_grpc.get_arpStub(channel=connect)
+    #通过stub调用服务端的Login_info方法
+    response = stub.Login_info(arphello_pb2.arpRequest(query_content="127.0.0.1"))
+    print("---------客户端收到信息----------")
+    print(response.arp_info)
+
+if __name__ == "__main__":
+    run()
---
+++ U04/GRPC/grpc_server.py
@@ -0,0 +1,40 @@
+import subprocess
+import grpc
+from concurrent import futures
+import arphello_pb2
+import arphello_pb2_grpc
+
+class do_it(arphello_pb2_grpc.get_arpServicer):
+    def __init__(self):
+        self.known_ip = "127.0.0.1"
+        
+    def Login_info(self, request, context):
+        #如果查询的是127.0.0.1的ARP信息，就可以做，否则就返回
+        if request.query_content == self.known_ip:
+            arp_data = self.get_arp_info()
+            return arphello_pb2.arpReply(arp_info=arp_data)
+        else:
+            print("查不到该IP的ARP信息")
+            return arphello_pb2.arpReply(arp_info="查不到该IP的ARP信息")
+        
+    def get_arp_info(self):
+        result = subprocess.run(['arp', '-a'], 
+                                capture_output=True, 
+                                text=True, 
+                                check=True)
+        return result.stdout
+
+
+def serve():
+    #创建gRPC服务
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    #从定义的服务中部署gRPC servicer
+    arphello_pb2_grpc.add_get_arpServicer_to_server(do_it(),server)
+    #启动服务器
+    server.add_insecure_port('localhost:10050')
+    server.start()
+    print("服务端已启动！")
+    server.wait_for_termination()
+
+if __name__ == "__main__":
+    serve()
