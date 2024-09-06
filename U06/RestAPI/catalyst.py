e4afb380d654819c8e33beae8ae08683149e5e47
Jackal <ccxxll23@163.com>
2024-09-06 17:02:08 +0800

    updated

---
+++ U06/RestAPI/catalyst.py
@@ -0,0 +1,81 @@
+import time
+import requests
+from requests.auth import HTTPBasicAuth
+
+# 禁用SSL警告
+requests.packages.urllib3.disable_warnings()
+
+class DNAC:
+    def __init__(self, url, username, password):
+        self.url = url
+        token = self.get_dnac_token(username, password)
+        self.headers = {
+            "Content-Type": "application/json",
+            "X-Auth-Token": token
+        }
+        self.protocol = 'icmp'
+        self.inclusions = ['INTERFACE-STATS',
+                           'DEVICE-STATS',
+                           'ACL-TRACE',
+                           'QOS-STATS']
+        self.ip_list = self.get_devices_ip()
+        self.fid = None
+
+    # 获取DNAC的认证Token
+    def get_dnac_token(self, username, password):
+        url = f"{self.url}/dna/system/api/v1/auth/token"
+        response = requests.post(url, auth=HTTPBasicAuth(username, password), verify=False)
+        token = response.json()["Token"]
+        return token
+
+    # 获取网络设备信息
+    def get_devices_ip(self):
+        url = f"{self.url}/dna/intent/api/v1/network-device"
+        response = requests.get(url, headers=self.headers, verify=False)
+        return [info['managementIpAddress'] for info in response.json()['response']]
+
+    # 获取路径追踪摘要
+    def get_path_trace_summary(self, dst_ip):
+        url = f"{self.url}/dna/intent/api/v1/flow-analysis"
+        response = requests.get(url, headers=self.headers, verify=False)
+        return response.json()['response']
+
+    # 获取路径追踪
+    def get_path_trace_result(self, flow_analysis_id):
+        url = f"{self.url}/dna/intent/api/v1/flow-analysis/{flow_analysis_id}"
+        response = requests.get(url, headers=self.headers, verify=False)
+        return response.json()['response']
+    
+    # 删除已有路径追踪进程
+    def del_old_flow_trace_task(self, ip_list):
+        # 获取已有路径追踪进程id
+        for ip in ip_list:
+            _path_trace_summary = self.get_path_trace_summary(ip)
+            old_flow_ids = [_summary['id'] for _summary in _path_trace_summary]
+            # 删除目标IP相关的已存在路径追踪进程
+            for old_flow_id in old_flow_ids:
+                url = f"{self.url}/dna/intent/api/v1/flow-analysis/{old_flow_id}"
+                response = requests.delete(url, headers=self.headers, verify=False)
+        return "消息: 历史路径追踪任务已删除"
+
+    # 开启路径追踪
+    def start_path_trace(self, src_ip, dst_ip):
+        url = f"{self.url}/dna/intent/api/v1/flow-analysis"
+        payload =  {"destIP": dst_ip,
+                    "sourceIP": src_ip,
+                    "protocol": self.protocol,
+                    "inclusions": self.inclusions,
+                   }
+        response = requests.post(url, json=payload, headers=self.headers, verify=False)
+        return response.json()['response']['flowAnalysisId']
+    
+    # 端到端路径获取
+    def trace_src_dst(self, src_ip, dst_ip):
+        # 两点之间开启新的路径追踪进程
+        self.fid = self.start_path_trace(src_ip, dst_ip)
+        # 等待进程跑完
+        time.sleep(10)
+        # 获取进程中的路径信息
+        path_infos = self.get_path_trace_result(self.fid)
+        return path_infos
+    
---
+++ U06/RestAPI/path_app.py
@@ -0,0 +1,94 @@
+import logging
+import time
+from catalyst import DNAC
+
+# 初始化日志
+logging.basicConfig(filename='catalyst.log',
+                    level=logging.INFO,
+                    format = '%(asctime)s  %(filename)s : %(levelname)s  %(message)s',
+                    datefmt = '%Y-%m-%d %A %H:%M:%S')
+
+logging.info('Catalyst Path Tracer App Start!')
+
+# DNAC的URL和凭据
+DNAC_URL = "https://sandboxdnac.cisco.com"
+USERNAME = "devnetuser"
+PASSWORD = "Cisco123!"
+
+dnac = DNAC(DNAC_URL, USERNAME, PASSWORD)
+
+
+# 全网追踪
+def trace_all():
+    # 删除旧的路径追踪进程
+    print("正在清空旧的监控进程，请稍后...")
+    dnac.del_old_flow_trace_task(dnac.ip_list)
+    # 设置一个源IP位置
+    off_set = 1
+    # 取所有不重复IP组合
+    for src_ip in dnac.ip_list:
+        for dst_ip in dnac.ip_list[off_set:]:
+            print("-" * 20)
+            print("正在追踪从{}到{}的路径信息:".format(src_ip, dst_ip))
+            # 进行两点之间的路径追踪
+            net_path = dnac.trace_src_dst(src_ip, dst_ip)
+            log_info = src_ip + "," + dst_ip + "," + ">".join(net_path) + "\n"
+            logging.info(log_info)
+            print(net_path)
+            print("-" * 20)
+            print("\n")
+        off_set += 1
+
+def a2b_monitor(src_ip, dst_ip):
+    # 删除旧的路径追踪进程
+    print("正在清空旧的监控进程，请稍后...")
+    dnac.del_old_flow_trace_task([src_ip, dst_ip])
+    print("开始对{}到{}之间进行链路监控\n".format(src_ip, dst_ip))
+    # 进行两点之间的路径追踪
+    path_infos = dnac.trace_src_dst(src_ip, dst_ip)
+    issues = analysis(src_ip, path_infos)
+    print(f'本次监控发现问题：{issues}')
+
+    # 循环监控时把下面的注释取消并继续编写巡检监控的可视化、数据存储等代码
+    '''
+    while True:
+        issues = analysis(src_ip, path_infos)
+        print(f'本次监控发现问题：{issues}')
+        time.sleep(60)
+        del path_infos
+        path_infos = dnac.get_path_trace_result(dnac.fid)
+    '''
+
+def analysis(src_ip, path_infos):
+    issues = []
+    for device_info in path_infos['networkElementsInfo']:
+        device_name = device_info['name']
+        logging.info(f'Start Checking {device_name}')
+        if 'ingressInterface' in device_info.keys():
+            device_int_status = device_info['ingressInterface']['physicalInterface']['interfaceStatistics'][
+                'adminStatus']
+        elif 'egressInterface' in device_info.keys():
+            device_int_status = device_info['egressInterface']['physicalInterface']['interfaceStatistics'][
+                'adminStatus']
+        else:
+            device_int_status = 'up'
+        device_total_mem = device_info['deviceStatistics']['memoryStatistics']['totalMemory']
+        device_used_mem = device_info['deviceStatistics']['memoryStatistics']['memoryUsage']
+        device_free_mem = device_total_mem - device_used_mem
+        log_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+        log_info = (log_time + "," + src_ip + "," + device_name +
+                    "," + device_int_status + "," + str(device_free_mem) + "\n")
+        logging.info(log_info)
+        if device_int_status != "up":
+            issue = f"{device_name}设备的接口出现问题，请尽快处理！"
+            if issue not in issues:
+                issues.append(issue)
+        if device_free_mem < 2000000000:
+            issue = f"{device_name}设备剩余内存较低，请尽快处理！"
+            if issue not in issues:
+                issues.append(issue)
+    return issues
+
+# 请分别测试下面的用例
+#trace_all()
+#a2b_monitor('10.10.20.177','10.10.20.178')
